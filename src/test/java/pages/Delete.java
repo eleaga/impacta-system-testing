d3e0daaf2e1c8d6f65ebc4289e9b7cfb8d540636
Luiz Henrique Bezerra <lbezerra@itelios.com>
2020-07-05 17:30:22 -0300

    create

---
+++ src/test/java/pages/Delete.java
@@ -0,0 +1,24 @@
+package pages;
+
+import support.DriverQA;
+
+public class Delete extends BasePage{
+
+    public Delete(DriverQA stepDriver) {
+        super(stepDriver);
+    }
+
+
+    public void openPage() {
+        String url = "http://agapito-server.herokuapp.com/processos";
+        driver.openURL(url);
+    }
+
+    public void deleteFirst() {
+        String selector = ".ls-table tr:first-child a.ls-btn-primary-danger";
+
+        driver.click("Apagar", selector);
+    }
+
+
+}
